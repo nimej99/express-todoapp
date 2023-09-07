f7798ec2255db122bf526200dc9800c90f204ac7
nimej99 <aaron9909@naver.com>
2023-09-07 16:03:32 +0900

    delete 09/07

--- server.js
+++ server.js
@@ -73,9 +73,6 @@ app.post('/add', async (요청, 응답) => {
       date: 요청.body.date
     });
 
-    console.log('저장완료');
-    console.log(result);
-
     // 게시물 갯수 업데이트
     await client.db('todoapp').collection('counter').updateOne(
       { name: '게시물갯수' },
@@ -88,6 +85,38 @@ app.post('/add', async (요청, 응답) => {
 
   } catch (에러) {
     console.error(에러);
+  } finally {
+    // 클라이언트 연결 닫기
+    await client.close();
+  }
+});
+
+app.delete('/delete', async (요청, 응답) => {
+  try {
+    console.log(요청.body);
+
+    // MongoDB 클라이언트 연결
+    await client.connect();
+
+    await client.db("todoapp").command({ ping: 1 });
+
+    // 요청에서 _id를 가져와서 정수로 변환
+    const postId = parseInt(요청.body._id);
+
+    // post 컬렉션에서 게시물 삭제
+    const 삭제결과 = await client.db('todoapp').collection('post').deleteOne({ _id: postId });
+
+    if (삭제결과.deletedCount === 1) {
+      console.log('삭제완료');
+      응답.status(200).send({ message: '성공' });
+    } else {
+      console.log('삭제 실패');
+      응답.status(400).send({ message: '삭제 실패' });
+    }
+
+  } catch (에러) {
+    console.error(에러);
+    응답.status(500).send({ message: '서버 오류' });
   } finally {
     // 클라이언트 연결 닫기
     await client.close();
