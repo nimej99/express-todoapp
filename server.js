ef016fde6b3132c3de227a809f97f946950f0ad1
nimj9909 <aaron9909@naver.com>
2023-09-11 20:34:33 +0900

    put method-override 09/11

--- server.js
+++ server.js
@@ -16,8 +16,12 @@ app.listen(8080, function () {
   console.log('litening on 8080')
 });
 
-app.use(express.urlencoded({ extended: true })) //post로 요청한 값 url 인코딩으로 알아서 해석해서 가져오기
+app.use(express.urlencoded({ extended: true })) //post로 요청한 값 url 인코딩으로 알아서 해석해서 가져오기 json 변형인듯
 app.use('/public', express.static('public')) //public 폴더 사용하겠다 선언
+
+//메소드 오버라이드 라이브러리로 html상에서 put, delete 사용가능
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 app.set('view engine', 'ejs');
 
@@ -60,15 +64,100 @@ app.get('/detail/:id', async function (요청, 응답) {
     // MongoDB 클라이언트 연결
     await client.connect();
 
+    // 요청 파라미터에서 _id를 정수로 변환
+    const postId = parseInt(요청.params.id);
+
+    // _id가 유효한지 확인
+    if (isNaN(postId)) {
+      throw new Error('유효하지 않은 게시물 ID');
+    }
+
+    // findOne 메서드를 사용하여 해당 _id의 문서 가져오기
+    const document = await client.db('todoapp').collection('post').findOne({ _id: postId });
+
+    // 문서가 없는 경우 처리
+    if (!document) {
+      throw new Error('게시물을 찾을 수 없음');
+    }
+
+    // 결과를 콘솔에 출력
+    console.log(document);
+
+    응답.render('detail.ejs', {
+      posts: document
+    });
+
+  } catch (에러) {
+    console.error(에러);
+
+    // 에러 메시지를 클라이언트에게 전달
+    응답.status(404).send('페이지를 찾을 수 없습니다.'); // 또는 다른 에러 처리 방식을 사용할 수 있음
+
+  } finally {
+    // 클라이언트 연결 닫기
+    await client.close();
+  }
+});
+
+
+app.get('/edit/:id', async function (요청, 응답) {
+  try {
+    // MongoDB 클라이언트 연결
+    await client.connect();
+
+    //파라미터를 정수로 변환
+    const postId = parseInt(요청.params.id);
+
+    // _id가 유효한지 확인
+    if (isNaN(postId)) {
+      throw new Error('유효하지 않은 게시물 ID');
+    }
+
     // find 메서드를 사용하여 모든 문서 가져오기
-    const documents = await client.db('todoapp').collection('post').findOne({ _id: parseInt(요청.params.id) });
+    const documents = await client.db('todoapp').collection('post').findOne({ _id: postId });
+
+    // 문서가 없는 경우 처리
+    if (!documents) {
+      throw new Error('게시물을 찾을 수 없음');
+    }
 
     // 결과를 콘솔에 출력
     console.log(documents);
 
-    응답.render('detail.ejs', {
+    응답.render('edit.ejs', {
       posts: documents
     });
+
+  } catch (에러) {
+    console.error(에러);
+
+    // 에러 메시지를 클라이언트에게 전달
+    응답.status(404).send('페이지를 찾을 수 없습니다.'); // 또는 다른 에러 처리 방식을 사용할 수 있음
+
+  } finally {
+    // 클라이언트 연결 닫기
+    await client.close();
+  }
+});
+
+app.put('/edit', async (요청, 응답) => {
+  try {
+    // MongoDB 클라이언트 연결
+    await client.connect();
+
+    await client.db("todoapp").command({ ping: 1 });
+
+    // post에 게시물 추가
+    const result = await client.db('todoapp').collection('post').updateOne({
+      _id: parseInt(요청.body._id)
+    }, {
+      $set: {
+        title: 요청.body.title,
+        date: 요청.body.date
+      }
+    });
+
+    응답.redirect('/list'); // 게시물 추가 후 목록 페이지로 리다이렉트
 
   } catch (에러) {
     console.error(에러);
