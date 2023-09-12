397920fe6ea19e94872f1b4b1daafd73cab73443
nimej99 <aaron9909@naver.com>
2023-09-12 19:43:46 +0900

    login session 09/12

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 const app = express();
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -16,6 +19,7 @@ app.listen(8080, function () {
   console.log('litening on 8080')
 });
 
+//app.use는 미들웨어로 응답,요청 사이에 함수를 실행하겠다는 뜻이다.
 app.use(express.urlencoded({ extended: true })) //post로 요청한 값 url 인코딩으로 알아서 해석해서 가져오기 json 변형인듯
 app.use('/public', express.static('public')) //public 폴더 사용하겠다 선언
 
@@ -23,14 +27,20 @@ app.use('/public', express.static('public')) //public 폴더 사용하겠다 선
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
+//세션부문
+app.use(session({ secret: '99', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//ejs 라이브러리
 app.set('view engine', 'ejs');
 
 app.get('/', function (요청, 응답) {
-  응답.sendFile(__dirname + '/index.html')
+  응답.render(__dirname + '/views/index.ejs')
 });
 
 app.get('/write', function (요청, 응답) {
-  응답.sendFile(__dirname + '/write.html')
+  응답.render(__dirname + '/views/write.ejs')
 });
 
 app.get('/list', async function (요청, 응답) {
@@ -139,6 +149,92 @@ app.get('/edit/:id', async function (요청, 응답) {
     await client.close();
   }
 });
+
+app.get('/login', function (요청, 응답) {
+  응답.render(__dirname + '/views/login.ejs')
+});
+
+//로컬방식 인증, 실패시 /fail로 이동
+app.post('/login', passport.authenticate('local', {
+  failureRedirect: '/fail',
+  successRedirect: '/'
+}), (요청, 응답) => {
+  응답.redirect('/')
+});
+
+//인증방법 = strategy를 사용해 인증검사
+passport.use(new LocalStrategy({
+  usernameField: 'id', //form name = id
+  passwordField: 'pw', //form name = pw
+  session: true, //세션 저장여부
+  passReqToCallback: false,
+}, async (입력한아이디, 입력한비번, done) => {
+  try {
+
+    await client.connect();
+
+    await client.db("todoapp").command({ ping: 1 });
+
+    let result = await client.db('todoapp').collection('login').findOne({ id: 입력한아이디 });
+
+    if (!result) return done(null, false, { message: '존재하지않는 아이디요' })
+
+    // 비밀번호 해싱을 사용하지 않으므로 단순 비교
+    if (입력한비번 === result.pw) {
+      return done(null, result); // 결과 = 세션 데이터 생성, 세션 id 정보 쿠키 생성
+    } else {
+      return done(null, false, { message: '비번틀렸어요' });
+    }
+
+
+  }
+  catch (에러) {
+    console.error(에러);
+  } finally {
+    // 클라이언트 연결 닫기
+    await client.close();
+  }
+}));
+
+//인증성공 했다면 세션생성
+passport.serializeUser(function (user, done) {
+  done(null, user.id); // 또는 사용자의 고유 식별자
+});
+
+passport.deserializeUser(async function (아이디, done) {
+  try {
+    // MongoDB 클라이언트 연결
+    await client.connect();
+
+    // MongoDB에서 사용자 정보 조회
+    const result = await client.db('todoapp').collection('login').findOne({ id: 아이디 });
+
+    if (result) {
+      done(null, result); // 사용자 객체를 세션에 저장
+    } else {
+      done(null, false); // 사용자를 찾지 못한 경우
+    }
+  } catch (에러) {
+    console.error(에러);
+    done(에러);
+  } finally {
+    // 클라이언트 연결 닫기
+    await client.close();
+  }
+});
+
+app.get('/mypage', isLogin, (요청, 응답) => {
+  console.log(요청.user)
+  응답.render(__dirname + '/views/mypage.ejs', { data: 요청.user })
+})
+
+function isLogin(요청, 응답, next) {
+  if (요청.user) {
+    next()
+  } else {
+    응답.send('로그인시 이용가능 합니다.')
+  }
+}
 
 app.put('/edit', async (요청, 응답) => {
   try {
